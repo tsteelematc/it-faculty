54fece76a796a497cd11fad06f02a1ec6a0b29da
Tom Steele <tsteele@madisoncollege.edu>
2020-05-30 11:48:26 -0500

    Semester navigation logic...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'it-faculty';
+
+  semesters = [
+    'Spring'
+    , 'Summer'
+    , 'Fall'
+  ];
+
+  displaySemesterIndex = 2;
+
+  get displaySemester() {
+    return this.semesters[this.displaySemesterIndex];
+  }
+
+  displayYear = 2020;
+
+  previousSemester() {
+      // First check semester to see if we have to switch the year.
+      if (this.displaySemester == 'Spring') {
+        this.displaySemesterIndex = 2;
+        this.displayYear--;
+      } 
+      else {
+        this.displaySemesterIndex--
+      }
+  }
+
+  nextSemester() {
+      // First check semester to see if we have to switch the year.
+      if (this.displaySemester == 'Fall') {
+        this.displaySemesterIndex = 0;
+        this.displayYear++;
+      } 
+      else {
+        this.displaySemesterIndex++
+      }
+  }
 }
