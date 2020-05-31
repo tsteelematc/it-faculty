a97f32d870b706906269f36b4e5b87abdf2d3cc8
Tom Steele <tsteele@madisoncollege.edu>
2020-05-30 19:56:13 -0500

    Summer 2020 data...

--- src/app/courses.service.ts
+++ src/app/courses.service.ts
@@ -45,7 +45,90 @@ export class CoursesService {
                   , faculty: 'Temple'
                 }
               ]
-              : []
+              : semester == 'Summer 2020'
+                ? [
+                  { 
+                      class: 'Exploration of Info Technology'
+                      , faculty: 'Milbauer, Nina M'
+                  }
+                  , { 
+                      class: 'Preparation for an IT Career'
+                      , faculty: 'Bethea, Kim M' 
+                  }
+                  , { 
+                      class: 'Preparation for an IT Career'
+                      , faculty: 'Close, Jeffrey R'
+                  }
+                  , { 
+                      class: 'Firewall/VPN Technologies'
+                      , faculty: 'Roth, Damian'
+                  }
+                  , { 
+                      class: 'IT Security Awareness'
+                      , faculty: 'Milbauer, Nina M'
+                  }
+                  , { 
+                      class: 'IT Security Awareness'
+                      , faculty: 'Gallus, Aaron T'
+                  }
+                  , { 
+                      class: 'Linux Server 2 (Security)'
+                      , faculty: 'Masino,Michael'
+                  }
+                  , { 
+                      class: 'Python Programming'
+                      , faculty: 'Johnson, Victor'
+                  }
+                  , { 
+                      class: 'Java Programming'
+                      , faculty: 'Waite, Paula'
+                  }
+                  , { 
+                      class: 'Intro Prog with JavaScript'
+                      , faculty: 'Rodas,Lori'
+                  }
+                  , { 
+                      class: 'Website Development-HTML5'
+                      , faculty: 'Sutfin, Temple'
+                  }
+                  , { 
+                      class: 'Advanced CSS'
+                      , faculty: 'Rodas,Lori'
+                  }
+                  , { 
+                      class: 'Advanced Javascript'
+                      , faculty: 'Witkowski, Bryan'
+                  }
+                  , { 
+                      class: 'Windows Server 1'
+                      , faculty: 'Jaedike, Dale'
+                  }
+                  , { 
+                      class: 'Enterprise Client'
+                      , faculty: 'Larsen McCarthy, Lana'
+                  }
+                  , { 
+                      class: 'Linux Server 1'
+                      , faculty: 'Snelling, Angela'
+                  }
+                  , { 
+                      class: 'Introduction to Databases'
+                      , faculty: 'Weishan, Matthew'
+                  }
+                  , { 
+                      class: 'Introduction to Databases'
+                      , faculty: 'McCullough, Kenneth'
+                  }
+                  , { 
+                      class: 'SQL Database Programming'
+                      , faculty: 'Steele, Thomas'
+                  }
+                  , { 
+                      class: 'Intro to Cloud Computing'
+                      , faculty: 'Chambers, Curtis'
+                  }                  
+                ]
+                : []
             )
           }
           , 1500
