67feb62eab8bbf2725c4d6cc0e9b262c8d15dfdb
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-01-02 17:18:53 -0600

    amplify init to generate aws-exports ! ! !
    And then eff with index.html to eliminate JS error...
    
    Nice, once it worked ! ! !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { MatListModule } from '@angular/material/list';
 
 import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
 import Amplify from 'aws-amplify';
-import awsconfig from '../aws-exports';
+import awsconfig from '../aws-exports.js';
 
 Amplify.configure(awsconfig);
 
