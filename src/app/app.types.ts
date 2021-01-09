4a5b8411b5e1433c07ecd84270a1ee1059101eea
Tom Steele <tsteele@madisoncollege.edu>
2021-01-09 07:57:20 -0600

    Commenting, hmm...

--- src/app/app.types.ts
+++ src/app/app.types.ts
@@ -1,26 +1,43 @@
+//
 // This one is key/data from DynamoDB query result...
+//
+// Maybe strongly type it later ? ? ?
+//
 type SemesterDataFromCloud = any[];
 
-// Each class in the array from DynamoDB results is one of these...
+//
+// Each class in the array from the cloud (DynamoDB) results is one of these...
+//
 type SemesterClassFromCloud = {
     class: string
     , faculty: string[];
 };
 
-// Data is 'cached' locally in a Map so as not to keep querying DynamoDB needlessly...
+//
+// Data is 'cached' locally in a Map so as not to keep querying the cloud (DynamoDB) needlessly...
+//
 type CachedSemesterData = Map<
     string
     , SemesterClassFromCloud[]
 >;
 
+//
+// Data shape for the Classes tab...
+//
+// Not, this had the sections hacked into the display string. This was done
+// differently for the Faculty tab...
+//
 type DisplayByClassData = {
     class: string
     , faculty: {
-      facultyWithSessionCountIfNecessary: string   // Hacked to show multiple sessions, for now ! ! !
+      facultyWithSessionCountIfNecessary: string
       , checked: boolean
     }[]
 };
   
+//
+// Data shape for the Faculty tab...
+//
 type DisplayByFacultyData = {
     faculty: string
     , classes: {
